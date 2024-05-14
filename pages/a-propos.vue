e6cfd3ca8f7609b114a37f4505aa06824969b7c7
Nicolas Walter <nicolaswalter2003@gmail.com>
2024-05-14 14:32:22 +0200

    chore(a-propos): Update text and improve formatting

--- pages/a-propos.vue
+++ pages/a-propos.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <div aria-hidden="true"
-      class="fixed -left-4 bottom-4 md:bottom-8 text-7xl sm:text-[8rem] sm:-left-8 md:text-[12rem] md:-left-12 lg:-left-16 lg:text-[15rem] select-none font-black text-gray-100 dark:text-gray-900">
+    <div
+      aria-hidden="true"
+      class="fixed -left-4 bottom-4 md:bottom-8 text-7xl sm:text-[8rem] sm:-left-8 md:text-[12rem] md:-left-12 lg:-left-16 lg:text-[15rem] select-none font-black text-gray-100 dark:text-gray-900"
+    >
       A propos
     </div>
     <section class="relative max-w-3xl mx-auto mb-10">
@@ -11,14 +13,19 @@
         <div class="space-y-12">
           <div v-for="info in infos">
             <div class="flex items-center -ml-6 sm:-ml-10 gap-3 sm:gap-6 mb-3">
-              <div class="flex items-center justify-center size-4 bg-black dark:bg-white rounded-full">
+              <div
+                class="flex items-center justify-center size-4 bg-black dark:bg-white rounded-full"
+              >
                 <div class="size-3 bg-white dark:bg-black rounded-full"></div>
               </div>
               <h2 class="text-lg font-semibold" :id="info.id">
                 {{ info.title }}
               </h2>
             </div>
-            <p v-for="paragraph in info.content" class="leading-relaxed text-gray-600 dark:text-gray-300 mb-3">
+            <p
+              v-for="paragraph in info.content"
+              class="leading-relaxed text-gray-600 dark:text-gray-300 mb-3"
+            >
               {{ paragraph }}
             </p>
           </div>
@@ -51,15 +58,15 @@ const infos = [
     title: "Développer pour le web.",
     content: [
       "Faire un site web, ce n'est pas juste mettre du texte, des images et des vidéos puis animer un peu le tout et faire joli. C'est bien plus que cela. Malgré le changement constant des technologies, la base du web reste indifférente aux nouveaux \"frameworks\" et \"librairies\". On sous-estime souvent une bonne connaissance du HTML pur. Oui, vous avez bien entendu, je parle du HTML. Aujourd'hui, on cherche des développeurs qui maitrisent le JavaScript, pour faire des sites dynamiques et interactifs. Mais utiliser un framework front et écrire du JavaScript dans tous les sens c'est bien beau, mais ce qui compte vraiment, c'est ce qui va être généré et utilisé par le navigateur.",
-      "C'est là où un bon \"markup\" devient utile : la sémantique est extrêmement importante, tout d'abord pour une question de SEO, car un contenu bien balisé sera plus mis en avant, mais aussi pour une question d'accessibilité et de practicité. Un site bien balisé et correctement implémenté, c'est un site qui sera affiché correctement en mode lecture, qui pourra être exploité par certaines extensions de navigateur, mais c'est aussi un site qui sera rendu fidèlement aux personnes utilisatrices de technologies d'assistance. Cette base est nécessaire afin d'avoir un site web accessible et utilisable par tous, et c'est ce que je m'efforce de faire dans mes projets.",
+      "C'est là où un bon \"markup\" devient utile : la sémantique est extrêmement importante, tout d'abord pour une question de SEO, car un contenu bien balisé sera plus mis en avant, mais aussi pour une question d'accessibilité et de practicité. Un site bien balisé et correctement implémenté, c'est un site qui sera affiché correctement en mode lecture, qui pourra être exploité par certaines extensions de navigateur, mais c'est aussi un site qui sera rendu fidèlement aux personnes utilisatrices de technologies d'assistance. Cette base est nécessaire, et c'est ce que je m'efforce de faire dans mes projets.",
     ],
   },
   {
     title: "L'accessibilité, une nécessité.",
     id: "accessibilite",
     content: [
-      "Comme expliqué précédemment, je me suis découvert au fil de mes années d'études et de travail un véritable engouement pour l'accessibilité du web. C'est pourquoi je m'attache à rendre mes projets accessibles au plus grand nombre. Cela passe par une bonne sémantique, mais aussi par une bonne utilisation des technologies. Par exemple, j'utilise en priorité des éléments natifs et n'utilise des librairies que lorsque cela est nécessaire, en y ajoutant des propriétés \"aria\". Cela permet d'avoir un site web plus léger, plus rapide, et plus accessible. L'ajout ou le masquage d'informations pour les utilisateurs de lecteurs d'écran, ainsi que la sémantique de la page, sont des aspects trop souvent négligés",
-      "Lors de mes travaux personnels, professionnels ou universitaires, je me suis formé à l'accessibilité du web, principalement par envie, en consultant les documentations officielles techniques en ligne comme le \"WCAG\", la plateforme de \"Mozilla\" et d'autres. J'ai aussi eu l'occasion de travailler sur des projets d'accessibilité professionnels, notamment la mise en conformité d'une application web complexe avec le RGAA. J'ai aussi eu l'occasion de vérifier officiellement mon niveau de compétences au travers de certifications reconnues.",
+      "Comme expliqué précédemment, je me suis découvert au fil de mes années d'études et de travail un véritable engouement pour l'accessibilité du web. Je m'attache donc à rendre mes projets accessibles : cela passe par une bonne sémantique, mais aussi par une utilisation adaptée des technologies. Par exemple, j'utilise en priorité des éléments HTML natifs et n'utilise le JavaScript que lorsque cela est nécessaire. Cela permet d'avoir un site web plus léger, plus rapide, et plus accessible. L'ajout ou le masquage d'informations pour les utilisateurs de lecteurs d'écran ainsi que l'ajout de propriété \"aria\" sont des aspects trop souvent négligés par les développeurs, mais qui sont pourtant essentiels pour une bonne accessibilité.",
+      "Lors de mes travaux personnels ou universitaires, je me suis auto-formé à l'accessibilité du web. J'ai aussi eu l'occasion de travailler sur des projets d'accessibilité professionnels, notamment la mise en conformité d'une Single Page Application complexe avec le RGAA. J'ai également eu l'occasion de vérifier officiellement mon niveau de compétences au travers de certifications reconnues.",
     ],
   },
   {
