2a815700b977edd2ce3bfdbd82a89796474e396e
Nicolas Walter <nicolaswalter2003@gmail.com>
2026-01-04 23:06:18 +0100

    feat(merci): add merci page with localized content and layout

---
+++ app/pages/merci.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+useHead({
+	title: $t("merciPage.title"),
+	meta: [
+		{
+			name: "description",
+			content: $t("merciPage.description"),
+		},
+	],
+});
+</script>
+
+<template>
+	<div>
+		<div
+			aria-hidden="true"
+			class="fixed bottom-0 -left-[8vmin] -z-10 hidden text-[22vmin] font-black text-[#EDEDED] select-none [writing-mode:vertical-lr] md:block dark:text-neutral-900"
+		>
+			{{ $t("merciPage.bgText") }}
+		</div>
+
+		<div class="mx-auto max-w-xl">
+			<div class="mb-14 flex flex-wrap gap-4">
+				<h1 class="text-3xl font-black lg:text-4xl">
+					{{ $t("merciPage.h1") }}
+				</h1>
+				<Icon name="lucide:send" class="size-8 lg:size-12" />
+			</div>
+			<p class="lg:text-lg">
+				{{ $t("merciPage.p") }}
+			</p>
+		</div>
+	</div>
+</template>
