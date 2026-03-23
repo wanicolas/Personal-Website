a3162ba7abe7b55e7d588c7c196783997b63c4db
Nicolas Walter <nicolaswalter2003@gmail.com>
2026-03-23 11:44:12 +0100

    feat(projets): replace SVG icon with Lucide icon for project links

--- app/pages/projets.vue
+++ app/pages/projets.vue
@@ -53,21 +53,10 @@ useSeoMeta({
 						<h2 class="inline text-lg font-semibold lg:text-xl">
 							{{ project.title }}
 						</h2>
-						<svg
-							aria-hidden="true"
-							class="mb-1 ml-2 inline size-4 shrink-0 stroke-2 transition motion-safe:group-hover/title:translate-x-1 motion-safe:group-hover/title:-translate-y-1"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-							/>
-						</svg>
+						<Icon
+							name="lucide:move-up-right"
+							class="-mb-0.5 ml-2 stroke-2 transition motion-safe:group-hover/title:translate-x-1 motion-safe:group-hover/title:-translate-y-1"
+						/>
 					</NuxtLink>
 					<div class="mb-2 flex flex-wrap items-center gap-2">
 						<span
